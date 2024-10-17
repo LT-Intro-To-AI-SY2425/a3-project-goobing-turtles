5e250ac296d22be7d7889c492e6e7bc8b1e66d9c
Meoward <meoward07@gmail.com>
2024-10-17 10:39:55 -0500

    aded boodks 14

--- books.py
+++ books.py
@@ -11,13 +11,84 @@ book_db: List[Tuple[str, str, str, int]] =[
         "frank herbert", 
         "science fiction", 
         1965, 
-    ) 
+    ), 
     ( 
         "crime and punishment", 
         "fyodor dostoevsky", 
         "literary fiction", 
         1866, 
-    )  
+    ), 
+    (
+        "things fall apart",
+        "Chinua Achebe",
+        "historical fiction",
+        1958
+    ),
+    (
+        "the great gastby",
+        "F. Scott Fitzgerald",
+        "literary fiction",
+        1925
+    ),
+    (
+        "moby dick",
+        "herman melville",
+        "adventure fiction",
+        1851
+    ),
+    (
+        "notes from underground"
+        "fyodor dostoevsky",
+        "literary fiction", 
+        1864
+    ),
+    (
+        "eragon",
+        "christopher paolini"
+        "fantacy",
+        2002
+    ),
+    (
+        "the young elites",
+        "Marie Lu",
+        "fantacy",
+        2014
+    ),
+    (
+        "brotherband",
+        "john flanagan",
+        "fantacy",
+        2011
+    ),
+    #10
+    (
+        "ranger's apprentice",
+        "john flanagan",
+        "fantacy",
+        2004
+    ),
+    (
+        "wings of fire",
+        "tui T. sutherland",
+        "fantacy",
+        2012
+    ),
+    (
+        "the stranger",
+        "albert camus",
+        "existential fiction",
+        1942
+    ),
+    (
+        "the odyessy"
+        "homer",
+        "myth",
+        -650
+    )
+    (
+        "don quihotes nsffafainai"
+    )
+
 ] #title author genre year
 
 
