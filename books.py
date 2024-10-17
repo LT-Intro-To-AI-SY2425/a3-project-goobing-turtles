53cc106bb7016299b2a4fecf6007d604c52d0cc8
Meoward <meoward07@gmail.com>
2024-10-17 10:21:35 -0500

    book

---
+++ books.py
@@ -0,0 +1,23 @@
+from typing import List, Tuple 
+book_db: List[Tuple[str, str, str, int]] =[ 
+    ( 
+        "harry potter", #title 
+        "jk rowling", #author 
+        "fantacy", #genre 
+        1997, 
+    ), 
+    ( 
+        "dune", 
+        "frank herbert", 
+        "science fiction", 
+        1965, 
+    ) 
+    ( 
+        "crime and punishment", 
+        "fyodor dostoevsky", 
+        "literary fiction", 
+        1866, 
+    )  
+] #title author genre year
+
+
